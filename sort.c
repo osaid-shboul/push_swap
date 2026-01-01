f784f40c0fbfbd5c42db37d8b3ffd8a3dfa08638
osaid <osaids1236@gmail.com>
2026-01-01 23:54:45 +0300

    add mor sort algorithems

--- sort.c
+++ sort.c
@@ -6,7 +6,7 @@
 /*   By: oalshbou <oalshbou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/01 21:52:45 by oalshbou          #+#    #+#             */
-/*   Updated: 2026/01/01 22:17:31 by oalshbou         ###   ########.fr       */
+/*   Updated: 2026/01/01 23:53:12 by oalshbou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,4 +61,64 @@ void	sort_five(t_stack **a, t_stack **b)
 	pa(a,b);
 	pa(a,b);
 }
+void	big_sort_to_b(t_stack **a, t_stack **b, int size)
+{
+	int	i;
+	int	range;
 
+	i = 0;
+	if(size <= 100)
+		range = 15;
+	else
+		range = 35;
+	while(*a)
+	{
+		if((*a)->index <= i)
+		{
+			pb(a, b);
+			i++;
+		}
+		else if ((*a)->index <= i + range)
+		{
+			pb(a, b);
+			rb(b);
+			i++;
+		}
+		else
+			ra(a);
+	}
+}
+
+void	big_sort_to_a(t_stack **a, t_stack **b)
+{
+	int	max;
+	int	pos;
+	int	size;
+
+	while(*b)
+	{
+		max = get_max_index(*b);
+		pos = get_target_position(*b, max);
+		size = stack_size(*b);
+		if(pos <= size / 2)
+		{
+			while((*b)->index != max)
+				rp(b);
+		}
+		else
+		{
+			while((*b)->index != max)
+				rrb(b);
+		}
+		pa(a, b);
+	}
+}
+
+void	big_sort(t_stack **a, t_stack **b)
+{
+	int	size;
+
+	size = stack_size(*a);
+	big_sort_to_b(a, b, size);
+	big_sort_to-a(a,b);
+}
